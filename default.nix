c81c8c1a01295c70ce23a553b1b792eb113b6151
Travis Athougies <travis@athougies.net>
2018-11-23 13:51:18 -0800

    Pull latest nixpkgs

--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ in {
             emailoverride = "";
             keepnr = 3;
             inputs = {
-              nixpkgs = { type = "git"; value = "git://github.com/kitecomputing/nixpkgs.git aeb470b626d391bcb206a568d68065326a512884"; emailresponsible = true; };
+              nixpkgs = { type = "git"; value = "git://github.com/kitecomputing/nixpkgs.git"; emailresponsible = true; };
               src = { type = "git"; value = "git://github.com/kitecomputing/kite-system.git"; emailresponsible = true; };
             };
           };
