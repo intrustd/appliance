f72b9f1689643b52814514277e224b815a80a7aa
Travis Athougies <travis@athougies.net>
2018-11-23 13:10:39 -0800

    Update emailresponsible

--- default.nix
+++ default.nix
@@ -17,7 +17,7 @@ in {
             keepnr = 3;
             inputs = {
               nixpkgs = { type = "git"; value = "git://github.com/kitecomputing/nixpkgs.git aeb470b626d391bcb206a568d68065326a512884"; emailresponsible = true; };
-              src = { type = "git"; value = "git://github.com/kitecomputing/kite-system.git"; email = true; };
+              src = { type = "git"; value = "git://github.com/kitecomputing/kite-system.git"; emailresponsible = true; };
             };
           };
         };
