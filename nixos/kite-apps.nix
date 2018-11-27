0921e71a5ab25866296c6596e0f41e4caa8e9ebd
Travis Athougies <travis@athougies.net>
2018-11-26 21:27:30 -0800

    Do not pass pkgs

--- nixos/kite-apps.nix
+++ nixos/kite-apps.nix
@@ -2,7 +2,6 @@
 
 let buildBundle = module:
       import (pkgs.kite.nix + /build-bundle.nix) {
-        inherit pkgs;
         systems = builtins.listToAttrs [ { name = pkgs.hostPlatform.config;
                                            value = pkgs; } ];
 	kite-app-module = module + /kite.nix;
