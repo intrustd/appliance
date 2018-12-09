feb7f80230c778880c2c5a9a132cf4a8ffeb2b55
Travis Athougies <travis@athougies.net>
2018-12-08 16:36:57 -0800

    Add restart option to kite-update

--- pkgs/kite-update/update.sh
+++ pkgs/kite-update/update.sh
@@ -1,6 +1,8 @@
 #! @runtimeShell@
 
 DIRECTION=switch
+RESTART=0
+NEEDS_RESTART=0
 ARG0=$0
 
 usage() {
@@ -11,6 +13,7 @@ usage() {
     echo "   -h, --help        Print this help message"
     echo "   --download-only   Only download the latest update"
     echo "   --boot-only       Only set this as boot"
+    echo "   --restart         Restart the system if the update requires"
     echo
     echo "For support, please e-mail hi@flywithkite.com"
 }
@@ -19,6 +22,7 @@ while (( $# )); do
     case "$1" in
         --download-only ) DIRECTION=download; shift ;;
         --boot-only ) DIRECTION=boot; shift ;;
+        --restart ) RESTART=1; shift ;;
         -h | --help ) usage; exit 0 ;;
         * ) usage; exit 1 ;;
     esac
@@ -37,6 +41,9 @@ echo "Upgrading to $(basename $latest_system)..."
 # nix-fetch $latest_system
 nix-store --realise $latest_system
 
+diff /run/current-system/etc/kite-boot-info $latest_system/etc/kite-boot-info >/dev/null
+NEEDS_RESTART="$?"
+
 case "$DIRECTION" in
     download)
         $latest_system/bin/switch-to-configuration dry-activate
@@ -49,10 +56,25 @@ case "$DIRECTION" in
 
     switch)
         echo "Switching to target configuration"
-        diff /run/current-system/etc/kite-boot-info $latest_system/etc/kite-boot-info >/dev/null
-        if [[ "$?" -neq 0 ]]; then
-            echo "WARNING: You will need to reboot your system after switch to this configuration"
-        fi
         $latest_system/bin/switch-to-configuration switch
+        ;;
+esac
+
+if [[ "$NEEDS_RESTART" -ne 0 ]]; then
+case "$DIRECTION" in
+    download|boot)
+        if [[ "$RESTART" -eq 1 ]]; then
+            echo "Not restarting because of --download-only or --boot-only"
+        fi
+        ;;
+
+    switch)
+        if [[ "$RESTART" -eq 1 ]]; then
+            echo "Restart requested. Restarting in 5 seconds (press Ctrl-C to interrupt)"
+            sleep 5
+            runit-init 6
+        else
+            echo "WARNING: A restart is required to switch to this configuration"
+        fi
         ;;
 esac
