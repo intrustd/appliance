b9facd0a5d088f0af8e121f71ba3ad5a3d13db6d
Travis Athougies <travis@athougies.net>
2018-12-18 09:23:34 -0800

    Bump admin app

--- pkgs/kite-static/default.nix
+++ pkgs/kite-static/default.nix
@@ -3,8 +3,8 @@
 let admin-app = fetchFromGitHub {
       owner = "kitecomputing";
       repo = "admin";
-      rev = "2b967d2c79b1a460d1659e42f7a862b46ccfbfc6";
-      sha256 = "1pfnq9v0cg42v9xz5xd1whn9l0iygdhlwrmvaljwxdkx8l2wyrkx";
+      rev = "6d0a81fafa992d4b57c3e92103bd3209d353df2e";
+      sha256 = "1r2jl61ha8sl2cfjsf7bq152aimpn73ksjb3kk042zaysb0f2lds";
     };
 
     nodeDeps = (((import (admin-app + /js) { pkgs = pkgs.buildPackages; nodejs = pkgs.buildPackages."nodejs-8_x"; }).shell.override { bypassCache = true; }).nodeDependencies);
