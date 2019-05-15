e75775bbe8f67a54a6833c91f05f22e78f34084f
Travis Athougies <travis@athougies.net>
2019-05-15 08:08:55 -0700

    Upgrade static

--- pkgs/intrustd-static/default.nix
+++ pkgs/intrustd-static/default.nix
@@ -3,8 +3,8 @@
 let admin-app = fetchFromGitHub {
       owner = "intrustd";
       repo = "admin";
-      rev = "b3c7ef55dcd71de10ea961fd9d3495e535129e27";
-      sha256 = "1adq4fn5nbi7ckmm2jypf93vs3g1755vl9w3anpl5lnq1x0kq65d";
+      rev = "d885a843623dc17e71e0e50e5dc3caaf0ef1a254";
+      sha256 = "0f5knqg6j0qd30vag7n5nmch23gc59dwv2anqwmh5dka6ck7z4nk";
     };
 
     nodeDeps = (((import (admin-app + /js) { pkgs = pkgs.buildPackages; nodejs = pkgs.buildPackages."nodejs-8_x"; }).shell.override { bypassCache = true; }).nodeDependencies);
